2280146353cf587c877185fe33f0c5c93bfad9a8
Miguel Ramos <return.infox@gmail.com>
2023-07-07 11:12:12 -0400

    nuevo test

--- src/test/java/com/kibernumacademy/devops/DevopsApplicationTests.java
+++ src/test/java/com/kibernumacademy/devops/DevopsApplicationTests.java
@@ -1,13 +1,40 @@
 package com.kibernumacademy.devops;
 
+import com.kibernumacademy.devops.entitys.Student;
+import com.kibernumacademy.devops.repositories.IStudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class DevopsApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+    @Autowired
+    private IStudentRepository repository;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testRepositorySavesStudents() {
+        // given
+        Student student3 = new Student("Sofia", "Ramos", "sofia@gmail.com");
+        Student student4 = new Student("Catalina", "Marquez", "catalina@gmail.com");
+
+        // when
+        repository.save(student3);
+        repository.save(student4);
+
+        // then
+        Optional<Student> foundMiguel = repository.findById(student3.getId());
+        Optional<Student> foundCamila = repository.findById(student4.getId());
+
+        assertTrue(foundMiguel.isPresent());
+        assertTrue(foundCamila.isPresent());
+    }
 }
